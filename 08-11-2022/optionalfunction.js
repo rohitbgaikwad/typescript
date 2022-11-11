87621c8e3109dee29ee1984560cdc6b8993dc8da
Rohitbapugaikwad <rohitbapugaikwad@gmail.com>
2022-11-11 08:25:30 +0530

    optionalfunction.ts
    optionalfunction.ts

---
+++ 08-11-2022/optionalfunction.js
@@ -0,0 +1,8 @@
+function display(id, name, role) {
+    if (role === void 0) { role = "admini"; }
+    console.log("Id", id);
+    console.log("Name", name);
+    console.log("Role", role);
+}
+display(1, "Dnyanesh", "Admin");
+display(1, "Dnyanesh");
---
+++ 08-11-2022/optionalfunction.ts
@@ -0,0 +1,8 @@
+function display(id:number, name:string, role:string="admini")
+{console.log("Id", id);console.log("Name", name);
+
+    console.log("Role", role);
+
+}
+display(1, "Dnyanesh", "Admin");
+display(1, "Dnyanesh");
